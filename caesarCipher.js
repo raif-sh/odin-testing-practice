1c8c934c163cd781abece86d24bd5112f0602fd3
Raif Shareef <raif.sh94@gmail.com>
2025-12-13 02:46:36 +0800

    caesarCipher function works with basic testing

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,12 +1,36 @@
-export function caesarCipher(str, key) {
-    const firstChar = str[0] + key;
-    console.log(firstChar)
-    if (str === 'xyz') {
-        return "abc"
-    } else if (str === 'HeLLo') {
-        return "khOOr"
-    } else if (str === 'Hello, World!') {
-        return 'Khoor, Zruog!'
+function caesarCipher(str, key) {
+    // initialize variable to save the cipher result
+    let result = '';
+
+    // for each character in inputString
+    for (let i = 0; i < str.length; i++){
+        // check if character is uppercase letter
+        if ((isUpperCase(str[i]))) {
+            // get base code A = 65
+            const baseCode = 'A'.charCodeAt(0);
+            // Calculate shifted code
+            const shiftedCode = (str[i].charCodeAt(0) - baseCode + key) % 26 + baseCode;
+            // append shifted code to result
+            result += String.fromCharCode(shiftedCode);
+        } else if (isLowerCase(str[i])) {
+            const baseCode = 'a'.charCodeAt(0);
+            const shiftedCode = (str[i].charCodeAt(0) - baseCode + key) % 26 + baseCode;
+            result += String.fromCharCode(shiftedCode);
+        } else {
+            result += str[i];
+        }
     }
+
+    return result;
 }
 
+function isUpperCase(char){
+    return char === char.toUpperCase() && char !== char.toLowerCase();
+};
+
+function isLowerCase(char){
+    return char === char.toLowerCase() && char !== char.toUpperCase();
+}
+
+
+export { caesarCipher };
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -5,10 +5,9 @@ test("ciphering xyz to abc with key 3", () => {
 })
 
 test("test case preservation", () => {
-    expect(caesarCipher('HeLLo', 3)).toBe('khOOr');
+    expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
 })
 
 test("punctuation, space and other non-alphanetical characters should remain unchanged", () => {
     expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
 })
-
